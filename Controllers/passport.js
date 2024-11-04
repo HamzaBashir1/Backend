5ab177e00bcd5e6a13990c728b8ca1e77c022d1d
Hamza <Hamzabashir02001@gmail.com>
2024-11-04 19:24:07 +0500

    fix google verify

--- Controllers/passport.js
+++ Controllers/passport.js
@@ -12,21 +12,48 @@ async (accessToken, refreshToken, profile, done) => {
   try {
     // Check if user already exists
     let user = await User.findOne({ email: profile.emails[0].value });
+    
     if (user) {
+      // Check if email is verified
+      if (!user.isVerified) {
+        return done(null, false, { message: "Please verify your email first" });
+      }
       return done(null, user);
     }
 
-    // Create a new user if not exists
+    // Create a new user if not exists and send verification email
     user = new User({
       email: profile.emails[0].value,
       name: profile.displayName,
-      photo: profile._json.picture,  // Save profile picture URL
+      photo: profile._json.picture, // Save profile picture URL
       role: 'guest', // Default role for new users
-      gender: 'other' // Default gender for new users
-      // password field is not used for Google OAuth users
+      gender: 'other', // Default gender for new users
+      isVerified: false // Set to false initially until verified
     });
     await user.save();
-    done(null, user);
+
+    // Generate verification token and send email
+    const verificationToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
+    const verificationLink = `${process.env.CLIENT_SITE_URL}/verify-email/guest/${verificationToken}`;
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: "sharjeelsohail279@gmail.com",
+        pass: "iyip nosn bwem gwer",
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: user.email,
+      subject: 'Email Verification',
+      text: `Please verify your email by clicking the following link: ${verificationLink}`
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    return done(null, false, { message: 'Verification email sent. Please check your inbox.' });
   } catch (err) {
     done(err);
   }
