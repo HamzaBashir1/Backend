82e3d9fa6d5d4ef69b57d9fc788bcd572f0e71bc
Hamza <Hamzabashir02001@gmail.com>
2025-02-11 14:08:18 +0500

    correct this

--- Routes/translationRoutes.js
+++ Routes/translationRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { translateAccommodation } from "../Controllers/TranslationController.js";
+import { translateAccommodation } from "../Controllers/translationController.js";
 
 const router = express.Router();
 
