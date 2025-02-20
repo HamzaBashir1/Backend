4174c24ac2c6a1a3dae33de80039194382c67535
Hamza <Hamzabashir02001@gmail.com>
2025-02-20 16:58:51 +0500

    add reservation if user delete accommodation auto delete reservation

--- utils/cleanupJob.js
+++ utils/cleanupJob.js
@@ -1,19 +1,27 @@
 import cron from 'node-cron';
 import Accommodation from '../models/Accommodation.js';
 import Host from '../models/Host.js';
+import Reservation from '../models/Reservation.js';
 
 // Schedule cleanup task to run every 5 minutes
 cron.schedule('*/1 * * * *', async () => {
-  console.log('Running cleanup task for accommodations...');
+  console.log('Running cleanup task for accommodations and reservations...');
 
   try {
     // Get all valid host IDs
     const hostIds = await Host.distinct('_id');
 
     // Delete accommodations where userId is not in the host collection
-    const result = await Accommodation.deleteMany({ userId: { $nin: hostIds } });
+    const resultAccommodations = await Accommodation.deleteMany({ userId: { $nin: hostIds } });
+    console.log(`Deleted ${resultAccommodations.deletedCount} accommodations with invalid userId.`);
 
-    console.log(`Deleted ${result.deletedCount} accommodations with invalid userId.`);
+    // Get all valid accommodation IDs
+    const validAccommodationIds = await Accommodation.distinct('_id');
+
+    // Delete reservations where accommodationId does not exist in Accommodation collection
+    const resultReservations = await Reservation.deleteMany({ accommodationId: { $nin: validAccommodationIds } });
+    console.log(`Deleted ${resultReservations.deletedCount} reservations with invalid accommodationId.`);
+    
   } catch (error) {
     console.error('Error during cleanup:', error);
   }
