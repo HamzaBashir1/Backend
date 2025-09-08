e523815e44b0b3cf72588467ad90ff6e319b5be1
Hamza <Hamzabashir02001@gmail.com>
2025-09-08 14:58:37 +0500

    revert changes

--- Routes/BlogRoutes.js
+++ Routes/BlogRoutes.js
@@ -9,11 +9,11 @@ router.post('/', createBlog);
 // Route for fetching all blog posts
 router.get('/', getBlogs);
 
-// Route for fetching a single blog post by ID
-router.get('/:id', getBlogById);
-
 // ✅ Get a blog by Slug (SEO friendly)
 router.get('/:slug', getBlogBySlug);
+
+// Route for fetching a single blog post by ID
+router.get('/:id', getBlogById);
 
 // Route for deleting a blog post by ID
 router.delete('/:id', deleteBlog);
